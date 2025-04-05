fdda2ec4f7091601a5f21f5e8fe3f7bc8cbab5f0
Ilya Siganov <ilya.siganov@gmail.com>
2025-04-05 22:31:21 +0600

    setup scene and movement

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var player_id: String = "-1"
-@export var speed = 14
+@export var speed = 7
 
 @onready var move_left = "move_left_" + player_id
 @onready var move_right = "move_right_" + player_id
@@ -11,8 +11,12 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	var velocity = Input.get_vector(move_left, move_right, move_forward, move_back)
-	if velocity.length() > 0.001:
-		print(player_id, velocity)
+	var direction = Input.get_vector(move_left, move_right, move_forward, move_back)
+	if direction.length() > 0.001:
+		print(player_id, direction)
 
+	target_velocity.z = direction.y * speed
+	target_velocity.x = direction.x * speed
 	
+	velocity = target_velocity
+	move_and_slide()
